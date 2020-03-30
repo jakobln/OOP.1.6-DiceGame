c81a0f2bf35d6684ba015ee3a735585c25299f45
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 13:48:18 +0200

    Initial commit

---
+++ DiceCup.cs
@@ -0,0 +1,36 @@
+﻿namespace DiceGame
+{
+    /// <summary>
+    /// This class represents a dice cup containing two dice.
+    /// </summary>
+    public class DiceCup
+    {
+        #region Instance fields
+        private Die _die1;
+        private Die _die2;
+        #endregion
+
+        #region Constructor
+        public DiceCup()
+        {
+            _die1 = new Die();
+            _die2 = new Die();
+        }
+        #endregion
+
+        // Implement a property TotalValue: the sum of 
+        // the face values of the dice in the cup
+        public int TotalValue
+        {
+            get { return _die1.FaceValue + _die2.FaceValue;}
+        }
+
+        // Implement a method Shake: all the dice in the cup should be rolled
+        //
+        public void Shake()
+        {
+            _die1.Roll();
+            _die2.Roll();
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,17 @@
+﻿namespace DiceGame
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            DiceCup dc = new DiceCup();
+
+            dc.Shake();
+            System.Console.WriteLine($"The sum of your dice is {dc.TotalValue}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
